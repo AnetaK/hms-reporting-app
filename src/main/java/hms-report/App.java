66b9d6a5780d142a670d7edfa2cc9379f18777e2
AnetaK <karbowska.aneta@gmail.com>
2017-03-18 13:26:38 +0100

    Initial

---
+++ src/main/java/hms-report/App.java
@@ -0,0 +1,13 @@
+package hms-report;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
