bd970682df6a48e87c1cada2231632d666bf2fe9
AnetaK <karbowska.aneta@gmail.com>
2017-03-18 13:39:50 +0100

    configs

--- src/main/java/pl.report/App.java
+++ src/main/java/pl.report/App.java
@@ -1,4 +1,4 @@
-package hms-report;
+package pl.report;
 
 /**
  * Hello world!
--- src/test/java/pl.report/AppTest.java
+++ src/test/java/pl.report/AppTest.java
@@ -1,4 +1,4 @@
-package hms-report;
+package pl.report;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
